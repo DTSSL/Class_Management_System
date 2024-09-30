812facdec4f2465bb1d7b5b5ffafb5c65239e9ce
sachith-priyamantha <89736781+sachithpriyamantha@users.noreply.github.com>
2024-09-30 09:03:55 +0530

    Update Slide bar

--- Class Management/frontend/src/pages/admin/SideBar.js
+++ Class Management/frontend/src/pages/admin/SideBar.js
@@ -1,16 +1,17 @@
-import * as React from 'react';
 import { Divider, ListItemButton, ListItemIcon, ListItemText, ListSubheader } from '@mui/material';
+import * as React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
-import HomeIcon from "@mui/icons-material/Home";
-import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
-import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import AnnouncementOutlinedIcon from '@mui/icons-material/AnnouncementOutlined';
-import ClassOutlinedIcon from '@mui/icons-material/ClassOutlined';
-import SupervisorAccountOutlinedIcon from '@mui/icons-material/SupervisorAccountOutlined';
-import ReportIcon from '@mui/icons-material/Report';
 import AssignmentIcon from '@mui/icons-material/Assignment';
+import ClassOutlinedIcon from '@mui/icons-material/ClassOutlined';
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import HomeIcon from "@mui/icons-material/Home";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import QrCodeScannerIcon from '@mui/icons-material/QrCodeScanner';
+import ReportIcon from '@mui/icons-material/Report';
+import SupervisorAccountOutlinedIcon from '@mui/icons-material/SupervisorAccountOutlined';
 
 const SideBar = () => {
     const location = useLocation();
@@ -23,6 +24,14 @@ const SideBar = () => {
                     </ListItemIcon>
                     <ListItemText primary="Home" />
                 </ListItemButton>
+
+                <ListItemButton component={Link} to="/Admin/students/student/attendance/:id">
+                    <ListItemIcon>
+                        <QrCodeScannerIcon color={location.pathname.startsWith ( "/Admin/students/student/attendance/:id") ? 'primary' : 'inherit'} />
+                    </ListItemIcon>
+                    <ListItemText primary="StudentAttendance" />
+                </ListItemButton>
+
                 <ListItemButton component={Link} to="/Admin/classes">
                     <ListItemIcon>
                         <ClassOutlinedIcon color={location.pathname.startsWith('/Admin/classes') ? 'primary' : 'inherit'} />
